319d28e2253ed48aaab3f80568ddc5159289b63f
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-10 12:44:48 +0530

    new JS file
    New JS file added for scripting to write new file and edit it by code

---
+++ newScript.js
@@ -0,0 +1,39 @@
+// Node.js Basics:
+// Introduction to Node.js
+// Installing Node.js and npm
+// Working with modules
+// File system operations
+// understanding HTTP module
+
+// nodeJS is a JS runtime environment
+
+// node & npm
+// working with node and npm
+
+const fs = require('fs');
+
+// writefile
+// fs.writeFile("hey.txt", "Hello Ankush", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");    
+// });
+
+// appendfile
+// fs.appendFile("hey.txt", ", Hello coder", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");    
+// });
+
+// rename
+// fs.rename("hey.txt", "myHey.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("file renamed"); 
+// });
+
+// copyfile
+fs.copyFile("myHey.txt", "./copy/copy.txt", function(err){
+    if(err) console.error(err);
+    else console.log("copied");
+});
+
+// unlink
