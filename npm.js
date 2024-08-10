354f45012a21e30ceb50e0b656c9cf4be8f21b79
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-10 17:50:14 +0530

    npm understanding
    understanding npm node modules, install, uninstall packages, updated package.json file also

---
+++ npm.js
@@ -0,0 +1,16 @@
+// NPM understanding
+
+// npm - node package manager
+
+//installing and uninstalling anything basics & advanced
+// install -> npm install/i <package_name>
+// uninstall -> npm uninstall <package_name>
+// installing particular versions
+
+// understanding node_modules
+
+// dependencies - packages and dependecies of packages
+
+// devdependencies - packages that only uses in development. After application is created and deployed we will not use these packages
+
+// scripts - understanding default scripts PATH and custom scripts
