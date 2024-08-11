35a58e722774245b4dac62dfc88579c2d2603404
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-11 15:13:43 +0530

    Routing
    Investigation study about nodemon and express and its usages and also learn request and response handling and error handling also middleware and routing and setting up a basic express application

--- express.js
+++ express.js
@@ -1,7 +1,44 @@
 // Express.js Framework:
+
 // Introduction to Express.js
+
+// express js is a package of npm
+// framework
+// express js manages everything from receiving the request and giving back the response
+
 // Setting up a basic Express application
 // Routing
-// Middleware
+// Middleware -> requestHandler is a middileware which is function(req, res{})
 // Request and response handling
 // Error handling
+
+const express = require('express');
+const app = express()
+
+app.use(function(req, res, next){
+    console.log("middleware started");
+    next();
+});
+app.use(function(req, res, next){
+    console.log("middleware started again");
+    next();
+});
+
+app.get('/', function(req, res){
+    res.send("Hello World");
+});
+
+app.get("/about", function(req, res){
+    res.send("This is the about page");
+});
+
+app.get('/profile', function(req, res, next){
+    return next(new Error("Something went wrong"));
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
+
+app.listen(3000);
